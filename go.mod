d27753f1b9faf4dc0bf06ee678fe39c7e1d983e4
tuannt20 <tuango.472000@gmail.com>
2022-12-12 23:18:13 +0700

    update build core

--- go.mod
+++ go.mod
@@ -10,18 +10,43 @@ require (
 )
 
 require (
+	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/elastic/go-licenser v0.3.1 // indirect
+	github.com/elastic/go-sysinfo v1.1.1 // indirect
+	github.com/elastic/go-windows v1.0.0 // indirect
 	github.com/go-zookeeper/zk v1.0.3 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jcchavezs/porto v0.1.0 // indirect
+	github.com/joeshaw/multierror v0.0.0-20140124173710-69b34d4ec901 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leekchan/accounting v1.0.0 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/procfs v0.0.0-20190425082905-87a4384529e0 // indirect
+	github.com/santhosh-tekuri/jsonschema v1.2.4 // indirect
 	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.elastic.co/apm v1.15.0 // indirect
+	go.elastic.co/apm/module/apmmongo v1.15.0 // indirect
+	go.elastic.co/fastjson v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
+	golang.org/x/mod v0.3.0 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.0.0-20200509030707-2212a7e161a5 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	howett.net/plist v0.0.0-20181124034731-591f970eefbb // indirect
 )
--- internal/zk/zk.go
+++ internal/zk/zk.go
@@ -10,7 +10,7 @@ import (
 // Connect ...
 func Connect() {
 	var (
-		uri = os.Getenv("ZOOKEREPER_URI")
+		uri = os.Getenv("ZOOKEEPER_URI")
 	)
 
 	// Connect
@@ -36,6 +36,56 @@ func server(envVars *config.ENV) {
 	envVars.App.Server = zk.GetStringValue(fmt.Sprintf("%s/server", appPrefix))
 	envVars.App.Port = zk.GetStringValue(fmt.Sprintf("%s/port", appPrefix))
 
+	// MongoDB
+	mongodbPrefix := getExternalPrefix("/mongodb/campaign")
+	envVars.MongoDB.URI = zk.GetStringValue(fmt.Sprintf("%s/uri", mongodbPrefix))
+	envVars.MongoDB.DBName = zk.GetStringValue(fmt.Sprintf("%s/db_name", mongodbPrefix))
+
+	envVars.MongoDB.ReplicaSet = zk.GetStringValue(fmt.Sprintf("%s/replica_set", mongodbPrefix))
+	envVars.MongoDB.CAPem = zk.GetStringValue(fmt.Sprintf("%s/ca_pem", mongodbPrefix))
+	envVars.MongoDB.CertPem = zk.GetStringValue(fmt.Sprintf("%s/cert_pem", mongodbPrefix))
+	envVars.MongoDB.CertKeyFilePassword = zk.GetStringValue(fmt.Sprintf("%s/cert_key_file_password", mongodbPrefix))
+	envVars.MongoDB.ReadPrefMode = zk.GetStringValue(fmt.Sprintf("%s/read_pref_mode", mongodbPrefix))
+
+	// NATS
+	natsPrefix := getExternalPrefix("/nats/campaign")
+	envVars.Nats.URL = zk.GetStringValue(fmt.Sprintf("%s/uri", natsPrefix))
+	envVars.Nats.Username = zk.GetStringValue(fmt.Sprintf("%s/user", natsPrefix))
+	envVars.Nats.Password = zk.GetStringValue(fmt.Sprintf("%s/password", natsPrefix))
+	envVars.Nats.APIKey = zk.GetStringValue(fmt.Sprintf("%s/api_key", natsPrefix))
+
+	// MongoDB_AUDIT
+	mongoAuditPrefix := getExternalPrefix("/mongodb/campaign_audit")
+	envVars.MongoAudit.Host = zk.GetStringValue(fmt.Sprintf("%s/host", mongoAuditPrefix))
+	envVars.MongoAudit.DBName = zk.GetStringValue(fmt.Sprintf("%s/db_name", mongoAuditPrefix))
+
+	// FileHost
+	commonPrefix := getCommonPrefix("")
+	envVars.FileHost = zk.GetStringValue(fmt.Sprintf("%s/file_host", commonPrefix))
+
+	// Authentication
+	authPrefix := getAppPrefix("/authentication")
+	envVars.SecretKey = zk.GetStringValue(fmt.Sprintf("%s/auth_secretkey", authPrefix))
+
+	// AUTH GG
+	envVars.EnableAuthenticationService = zk.GetStringValue(fmt.Sprintf("%s/authentication_google/enable", adminPrefix))
+
+	// Redis
+	redisPrefix := getExternalPrefix("/redis/campaign")
+	envVars.Redis.URI = zk.GetStringValue(fmt.Sprintf("%s/uri", redisPrefix))
+	envVars.Redis.Password = zk.GetStringValue(fmt.Sprintf("%s/auth/password", redisPrefix))
+
+}
+
+func getCommonPrefix(group string) string {
+	return fmt.Sprintf("%s%s", config.GetENV().ZookeeperPrefixCommon, group)
+}
+
+func getExternalPrefix(group string) string {
+	return fmt.Sprintf("%s%s", config.GetENV().ZookeeperPrefixExternal, group)
+}
+
+func commonValues(envVars *config.ENV) {
 }
 
 func getAppPrefix(group string) string {
@@ -44,7 +94,4 @@ func getAppPrefix(group string) string {
 
 func getAdminPrefix(group string) string {
 	return fmt.Sprintf("%s%s", config.GetENV().ZookeeperPrefixAdmin, group)
-}
-
-func commonValues(envVars *config.ENV) {
 }
