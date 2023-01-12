9fbdd05ba5637207df4a58faea332b360cb1e2aa
tuannt20 <tuango.472000@gmail.com>
2023-01-12 15:59:39 +0700

    :recycle:Update

--- pkg/admin/model/request/category.go
+++ pkg/admin/model/request/category.go
@@ -46,11 +46,11 @@ func (m CategoryBodyCreate) ConvertToBSON() mgexpense.Category {
 
 // CategoryAll ...
 type CategoryAll struct {
-	Page    int64  `json:"page"`
-	Limit   int64  `json:"limit"`
-	Keyword string `json:"keyword"`
-	Status  string `json:"status"`
-	Type    string `json:"type"`
+	Page    int64  `query:"page"`
+	Limit   int64  `query:"limit"`
+	Keyword string `query:"keyword"`
+	Status  string `query:"status"`
+	Type    string `query:"type"`
 }
 
 // Validate ...
@@ -70,7 +70,7 @@ func (m CategoryAll) Validate() error {
 	return validation.ValidateStruct(&m,
 		validation.Field(&m.Page, is.UTFLetterNumeric.Error(response.CommonPageInvalid)),
 		validation.Field(&m.Limit, is.UTFLetterNumeric.Error(response.CommonLimitInvalid)),
-		validation.Field(&m.Type, validation.In(types...).Error(errorcode.CategoryIsInvalid)),
+		validation.Field(&m.Type, validation.In(types...).Error(errorcode.CategoryTypeIsInvalid)),
 		validation.Field(&m.Status, validation.In(statuses...).Error(errorcode.CategoryStatusIsInvalid)),
 	)
 }
