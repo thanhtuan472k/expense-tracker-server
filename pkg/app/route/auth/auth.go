68f3a10c5ac309395f1c34287949f1058fbdfd93
tuannt20 <tuango.472000@gmail.com>
2023-03-10 17:41:59 +0700

    :sparkles: Add api user me
    Add api user me

--- pkg/app/route/auth/auth.go
+++ pkg/app/route/auth/auth.go
@@ -1,1 +1,20 @@
 package routeauth
+
+import (
+	"expense-tracker-server/external/response"
+	"expense-tracker-server/external/util/echocontext"
+	"github.com/labstack/echo/v4"
+)
+
+// RequiredLogin ...
+func RequiredLogin(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		user := echocontext.GetCurrenUserByToken(c.Get("user"))
+		if user == nil || user.ID == "" {
+			return response.R403(c, echo.Map{}, response.CommonForbidden)
+		}
+
+		c.Set("user", user.ID)
+		return next(c)
+	}
+}
