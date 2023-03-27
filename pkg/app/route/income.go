ab5a5200d4bed4819e8303d6913561466c650e73
tuannt20 <tuango.472000@gmail.com>
2023-03-27 09:22:58 +0700

    :sparkles: feat: Add required login route
    feat: Add required login route

--- pkg/app/route/income.go
+++ pkg/app/route/income.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"expense-tracker-server/pkg/app/handler"
+	routeauth "expense-tracker-server/pkg/app/route/auth"
 	routevalidation "expense-tracker-server/pkg/app/route/validation"
 	"github.com/labstack/echo/v4"
 )
@@ -9,7 +10,7 @@ import (
 // income ...
 func income(e *echo.Group) {
 	var (
-		g = e.Group("/incomes")
+		g = e.Group("/incomes", routeauth.RequiredLogin)
 		h = handler.Income{}
 		v = routevalidation.Income{}
 	)
